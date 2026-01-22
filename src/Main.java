7ddff217e8af40331cf29e047cbf17c567441043
Assem <asemseilbekovaa@gmail.com>
2026-01-22 14:32:34 +0500

    fc

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("ФИО: Сеильбекова Асем Амирхановна");
+        System.out.println("Группа: ИС 24-21");
+        System.out.println("Специальность: Информационные системы");
+
+    }
+}
